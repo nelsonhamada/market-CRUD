093e94f620cdbed589a5a52941c04a0f3fccdc07
Nelson Hamada <fumiyuki.hamada@gmail.com>
2023-06-20 16:53:35 -0300

    Initial commit.

---
+++ CRUD/src/App.tsx
@@ -0,0 +1,18 @@
+import { ReactElement } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css'
+// import Main from './pages/main/Main';
+
+function App(): ReactElement {
+  
+  return (
+    <>
+    <p>Teste </p>
+      <Routes>
+        {/* <Route path='/' element={ <Main /> } /> */}
+      </Routes>
+    </>
+  )
+}
+
+export default App
